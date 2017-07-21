3e70bc605b496307d1a43ac748b902b296e5d1e1
shangxinbo <314911714@qq.com>
2017-07-21 13:29:32 +0800

    添加待办事项

---
+++ src/renderer/components/todos/add.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <md-input-container>
+            <label>待办事项值</label>
+            <md-input v-model="initialValue"></md-input>
+        </md-input-container>
+        <md-button class="md-raised md-primary md-fab-bottom-center">提交</md-button>
+    </div>
+</template>
--- src/renderer/router.js
+++ src/renderer/router.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import todo from './components/todos/index.vue'
+import add from './components/todos/add.vue'
 
 Vue.use(Router)
 
 let mRouter = new Router({
     routes: [
-        { path: '/', name: 'todo', component: todo }
+        //{ path: '/', name: 'todo', component: todo }
+        { path: '/', name: 'add', component: add }
     ]
 })
 
