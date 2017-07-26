65450d484b536520666c4f69744ce4223eff040d
shangxinbo <314911714@qq.com>
2017-07-26 10:14:36 +0800

    列表页

--- src/renderer/components/todos/index.vue
+++ src/renderer/components/todos/index.vue
@@ -1,58 +1,49 @@
 <template>
     <div>
         <md-list>
-            <md-list-item>
-                <span>News</span>
-
-                <md-list-expand>
-                    <md-list>
-                        <md-list-item class="md-inset">World</md-list-item>
-                        <md-list-item class="md-inset">Americas</md-list-item>
-                        <md-list-item class="md-inset">Europe</md-list-item>
-                    </md-list>
-                </md-list-expand>
-            </md-list-item>
-
-            <md-list-item>
-                <span>Games</span>
-
-                <md-list-expand>
-                    <md-list>
-                        <md-list-item class="md-inset">Console</md-list-item>
-                        <md-list-item class="md-inset">PC</md-list-item>
-                        <md-list-item class="md-inset">Phone</md-list-item>
-                    </md-list>
-                </md-list-expand>
-            </md-list-item>
-
-            <md-list-item>
-                <span>Video</span>
-
-                <md-list-expand>
-                    <md-list>
-                        <md-list-item class="md-inset">Humor</md-list-item>
-                        <md-list-item class="md-inset">Music</md-list-item>
-                        <md-list-item class="md-inset">Movies</md-list-item>
-                        <md-list-item class="md-inset">TV Shows</md-list-item>
-                    </md-list>
-                </md-list-expand>
-            </md-list-item>
-
-            <md-list-item>
-                <span>Shop</span>
+            <md-list-item v-for="(item,index) in list" :key="index">
+                <template v-if="typeof(item)=='string'">
+                    <span>{{item}}</span>
+                </template>
+                <template v-else>
+                    <span>{{item.father}}</span>
+                    <md-list-expand>
+                        <md-list v-for="child in item.children">
+                            <md-list-item class="md-inset">{{child}}</md-list-item>
+                        </md-list>
+                    </md-list-expand>
+                </template>
             </md-list-item>
         </md-list>
     </div>
 </template>
 <script>
     export default {
-        data(){
-            return {}
+        data() {
+            return {
+                list: []
+            }
         },
-        beforeRouteEnter(to,from,next){
-            next(vm =>{
-                vm.$store.commit('CHANGE_TYPE',1)
+        created() {
+            this.$ajax({
+                url: 'http://localhost:3000/todos/list',
+                data: {
+                    type: 0
+                },
+                success: data => {
+                    if (data.code && data.code == 200) {
+                        this.list = data.data
+                    } else {
+                        this.list = []
+                    }
+                }
+            })
+        },
+        beforeRouteEnter(to, from, next) {
+            next(vm => {
+                vm.$store.commit('CHANGE_TYPE', 1)
             })
         }
     }
+
 </script>
