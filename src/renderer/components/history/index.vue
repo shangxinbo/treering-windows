a7c841f440b6d43f8a31fb7ebd179cad91f57f86
shangxinbo <314911714@qq.com>
2017-07-27 19:31:59 +0800

    历史记录为空

--- src/renderer/components/history/index.vue
+++ src/renderer/components/history/index.vue
@@ -1,20 +1,28 @@
 <template>
-    <md-table>
-        <md-table-header>
-            <md-table-row>
-                <md-table-head>任务</md-table-head>
-                <md-table-head>处理时间</md-table-head>
-                <md-table-head>完成/放弃</md-table-head>
-            </md-table-row>
-        </md-table-header>
-        <md-table-body>
-            <md-table-row v-for="(row, index) in list" :key="index">
-                <md-table-cell>{{row.text}}</md-table-cell>
-                <md-table-cell>{{row.end_time|timeFormat}}</md-table-cell>
-                <md-table-cell>{{row.status|statusText}}</md-table-cell>
-            </md-table-row>
-        </md-table-body>
-    </md-table>
+    <md-layout>
+        <md-table>
+            <md-table-header>
+                <md-table-row>
+                    <md-table-head>任务</md-table-head>
+                    <md-table-head>处理时间</md-table-head>
+                    <md-table-head>完成/放弃</md-table-head>
+                </md-table-row>
+            </md-table-header>
+            <md-table-body>
+                <md-table-row v-for="(row, index) in list" :key="index">
+                    <md-table-cell>{{row.text}}</md-table-cell>
+                    <md-table-cell>{{row.end_time|timeFormat}}</md-table-cell>
+                    <md-table-cell>{{row.status|statusText}}</md-table-cell>
+                </md-table-row>
+            </md-table-body>
+        </md-table>
+        <md-layout v-if="list.length<=0" md-column style="align-items: center;justify-content: center;min-height:500px;">
+            <md-button md-flex="1" disabled>
+                <i style="color:#b9b9b9" class="md-icon md-size-4x md-primary material-icons">folder open</i>
+            </md-button>
+            <div md-flex="1" style="text-align:center;color:#565656">暂时没有，请添加</div>
+        </md-layout>
+    </md-layout>
 </template>
 <script>
     import moment from 'moment'
