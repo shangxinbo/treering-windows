7fe0efa356806fcd155180a127be085115741954
shangxinbo <314911714@qq.com>
2017-08-02 18:21:04 +0800

    交换位置问题

--- src/renderer/components/todos/index.vue
+++ src/renderer/components/todos/index.vue
@@ -1,43 +1,45 @@
 <template>
-    <ul class="md-list md-theme-default" @drop="drop($event,-1)" @dragover="allowDrop($event)" >
-        <li class="md-list-item" draggable="true" v-for="(item,index) in list" :key="index" :class="{'md-list-item-expand':typeof(item)!='string'}"
-            @dragstart="drag(index,-1,$event)" @click="toggle(item,$event)">
-            <div class="md-list-item-container" v-if="typeof(item)=='string'">
-                <i aria-hidden="true" class="md-icon md-theme-default material-icons" @click.stop="add(index)">add</i>
-                <span>{{item}}</span>
-                <md-button class="md-icon-button" @click="del(index,-1)">
-                    <md-icon>delete sweep</md-icon>
-                </md-button>
-            </div>
-            <template v-else>
-                <div class="md-list-item-container md-button">
+    <div>
+        <ul v-if="list.length>0" class="md-list md-theme-default" @drop="drop($event,-1)" @dragover="allowDrop($event)" >
+            <li class="md-list-item" draggable="true" v-for="(item,index) in list" :key="index" :class="{'md-list-item-expand':typeof(item)!='string'}"
+                @dragstart="drag(index,-1,$event)" @click="toggle(item,$event)">
+                <div class="md-list-item-container" v-if="typeof(item)=='string'">
                     <i aria-hidden="true" class="md-icon md-theme-default material-icons" @click.stop="add(index)">add</i>
-                    <span>{{item.father}}</span>
-                    <i aria-hidden="true" class="md-icon md-list-expand-indicator md-theme-default material-icons">keyboard_arrow_down</i>
+                    <span>{{item}}</span>
+                    <md-button class="md-icon-button" @click="del(index,-1)">
+                        <md-icon>delete sweep</md-icon>
+                    </md-button>
                 </div>
-                <div class="md-list-expand" style="margin-bottom: -192px;">
-                    <div class="md-list-expand-container">
-                        <ul class="md-list md-theme-default" @click.stop @drop.stop="drop($event,index)" @dragover="allowDrop($event)">
-                            <li class="md-list-item md-inset" draggable="true" v-for="(child,code) in item.children" @dragstart.stop="drag(code,index,$event)">
-                                <div class="md-list-item-container">
-                                    <span>{{child}}</span>
-                                    <md-button class="md-icon-button" @click="del(code,index)">
-                                        <md-icon>delete sweep</md-icon>
-                                    </md-button>
-                                </div>
-                            </li>
-                        </ul>
+                <template v-else>
+                    <div class="md-list-item-container md-button">
+                        <i aria-hidden="true" class="md-icon md-theme-default material-icons" @click.stop="add(index)">add</i>
+                        <span>{{item.father}}</span>
+                        <i aria-hidden="true" class="md-icon md-list-expand-indicator md-theme-default material-icons">keyboard_arrow_down</i>
                     </div>
-                </div>
-            </template>
-        </li>
-        <md-layout v-if="list.length<=0" md-column style="align-items: center;justify-content: center;min-height:500px;">
+                    <div class="md-list-expand" style="margin-bottom: -192px;">
+                        <div class="md-list-expand-container">
+                            <ul class="md-list md-theme-default" @click.stop @drop.stop="drop($event,index)" @dragover="allowDrop($event)">
+                                <li class="md-list-item md-inset" draggable="true" v-for="(child,code) in item.children" @dragstart.stop="drag(code,index,$event)">
+                                    <div class="md-list-item-container">
+                                        <span>{{child}}</span>
+                                        <md-button class="md-icon-button" @click="del(code,index)">
+                                            <md-icon>delete sweep</md-icon>
+                                        </md-button>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </template>
+            </li>
+        </ul>
+        <md-layout v-else md-column style="align-items: center;justify-content: center;min-height:500px;">
             <md-button md-flex="1" disabled>
                 <i style="color:#b9b9b9" class="md-icon md-size-4x md-primary material-icons">folder open</i>
             </md-button>
             <div md-flex="1" style="text-align:center;color:#565656">暂时没有，请添加</div>
         </md-layout>
-    </ul>
+    </div>
 </template>
 <script>
     export default {
